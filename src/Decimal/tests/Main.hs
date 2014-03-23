c102ef6dfe7d1e5dd4d7496e573c20ae563b6343
Paul Johnson <paul@cogito.org.uk>
2014-03-23 16:44:41 +0000

    Added test case for DecimalConvert with overflow.

--- src/Decimal/tests/Main.hs
+++ src/Decimal/tests/Main.hs
@@ -218,7 +218,9 @@ tests = [
                 testCase "1.0 * pi"      (dec 1 31    @=? dec 1 10 *. piD),
                 testCase "1.23 * pi"     (dec 2 386   @=? dec 2 123 *. piD),
                 testCase "Decimal to DecimalRaw Int" 
-                                         (decimalConvert (dec 2 123) @=? dec1 2 123),
+                                         (decimalConvert (dec 2 123) @=? Just (dec1 2 123)),
+                testCase "decimalConvert overflow prevention"
+                                         (decimalConvert (1/3) @=? (Nothing :: Maybe (DecimalRaw Int))),
                 testCase "1.234 to rational" (1234 % 1000 @=? toRational (dec 3 1234)),
                 testCase "fromRational (1%10) for DecimalRaw Int"  -- Fixed bug #3
                                          (let v :: DecimalRaw Int
